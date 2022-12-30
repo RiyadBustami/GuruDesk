153606ebe4d644d15a06f9a74b4483706c69270e
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-12-30 20:04:33 +0200

    Login, Register, Logout in usercontroller, cookies

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const { User } = require('../models/user.model');
+const jwt = require("jsonwebtoken");
 
 module.exports.createUser = (request, response) => {
     User.create(request.body)
@@ -34,7 +35,45 @@ module.exports.deleteUser = (request, response) => {
 module.exports.register = (request, response) => {
     User.create(request.body)
         .then(user => {
-            response.json({msg: "Registered Successfully!", user: user})
+
+            const userToken = jwt.sign({
+                id: user._id
+            }, process.env.SECRET_KEY);
+
+            response
+                .cookie("usertoken", userToken, secret, {
+                    httpOnly: true
+                })
+                .json({ msg: "Registered successfully!", user: user });
         })
         .catch(err => response.json(err));
+}
+
+module.exports.login = async (request, response) => {
+    const user = await User.findOne({ email: request.body.email });
+
+    if (user === null) {
+        return response.sendStatus(400);
+    }
+
+    const correctPassword = await bcrypt.compare(request.body.password, user.password);
+
+    if (!correctPassword) {
+        return response.sendStatus(400);
+    }
+
+    const userToken = jwt.sign({
+        id: user._id
+    }, process.env.SECRET_KEY);
+
+    response
+        .cookie("usertoken", userToken, secret, {
+            httpOnly: true
+        })
+        .json({ msg: "Logged in successfully!" });
+}
+
+module.exports.logout = (request, response) => {
+    response.clearCookie('usertoken');
+    response.sendStatus(200);
 }
