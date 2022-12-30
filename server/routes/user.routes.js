295807d14433fe584e0f0a3cc67646124e680df6
SaeedQatanani <saeedqatananil@gmail.com>
2022-12-30 20:19:19 +0200

    added routes in user routes

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,4 +4,7 @@ module.exports = app => {
     app.post("/api/register", UserController.register);
     app.post("/api/login", UserController.login);
     app.get("/api/users", authenticate, UserController.getAll);
+    app.get('/api/users/:id', UserController.getUser);
+    app.put('/api/users/:id', UserController.updateUser);
+    app.delete('/api/users/:id', UserController.deleteUser);
 }
